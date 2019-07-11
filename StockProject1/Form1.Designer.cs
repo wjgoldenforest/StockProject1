b8d701bc2f38ea45d598fdeaa58359c0b9f35671
wjgoldenforest <cmumism@naver.com>
2019-07-11 23:15:09 +0900

    first commit

---
+++ StockProject1/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace StockProject1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.axKHOpenAPI1 = new AxKHOpenAPILib.AxKHOpenAPI();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.loginStateButton = new System.Windows.Forms.Button();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.idLabel = new System.Windows.Forms.Label();
+            this.accountCntLabel = new System.Windows.Forms.Label();
+            this.accountListLabel = new System.Windows.Forms.Label();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axKHOpenAPI1
+            // 
+            this.axKHOpenAPI1.Enabled = true;
+            this.axKHOpenAPI1.Location = new System.Drawing.Point(1, 576);
+            this.axKHOpenAPI1.Name = "axKHOpenAPI1";
+            this.axKHOpenAPI1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axKHOpenAPI1.OcxState")));
+            this.axKHOpenAPI1.Size = new System.Drawing.Size(79, 25);
+            this.axKHOpenAPI1.TabIndex = 0;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(18, 12);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(114, 49);
+            this.loginButton.TabIndex = 1;
+            this.loginButton.Text = "로그인";
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // loginStateButton
+            // 
+            this.loginStateButton.Location = new System.Drawing.Point(156, 13);
+            this.loginStateButton.Name = "loginStateButton";
+            this.loginStateButton.Size = new System.Drawing.Size(114, 48);
+            this.loginStateButton.TabIndex = 2;
+            this.loginStateButton.Text = "로그인 상태";
+            this.loginStateButton.UseVisualStyleBackColor = true;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(941, 9);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(44, 18);
+            this.nameLabel.TabIndex = 3;
+            this.nameLabel.Text = "이름";
+            // 
+            // idLabel
+            // 
+            this.idLabel.AutoSize = true;
+            this.idLabel.Location = new System.Drawing.Point(1003, 9);
+            this.idLabel.Name = "idLabel";
+            this.idLabel.Size = new System.Drawing.Size(22, 18);
+            this.idLabel.TabIndex = 4;
+            this.idLabel.Text = "ID";
+            // 
+            // accountCntLabel
+            // 
+            this.accountCntLabel.AutoSize = true;
+            this.accountCntLabel.Location = new System.Drawing.Point(1097, 9);
+            this.accountCntLabel.Name = "accountCntLabel";
+            this.accountCntLabel.Size = new System.Drawing.Size(62, 18);
+            this.accountCntLabel.TabIndex = 7;
+            this.accountCntLabel.Text = "계좌수";
+            // 
+            // accountListLabel
+            // 
+            this.accountListLabel.AutoSize = true;
+            this.accountListLabel.Location = new System.Drawing.Point(941, 32);
+            this.accountListLabel.Name = "accountListLabel";
+            this.accountListLabel.Size = new System.Drawing.Size(74, 18);
+            this.accountListLabel.TabIndex = 8;
+            this.accountListLabel.Text = "계좌_list";
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.searchTextBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.searchTextBox.Location = new System.Drawing.Point(317, 22);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(163, 28);
+            this.searchTextBox.TabIndex = 11;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(486, 12);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 45);
+            this.searchButton.TabIndex = 12;
+            this.searchButton.Text = "검색";
+            this.searchButton.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.chart, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(15, 80);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 65F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1135, 496);
+            this.tableLayoutPanel1.TabIndex = 13;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 30;
+            this.dataGridView.Size = new System.Drawing.Size(1129, 316);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // chart
+            // 
+            this.chart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.AxisX.IsReversed = true;
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            this.chart.Location = new System.Drawing.Point(3, 325);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Candlestick;
+            series1.Name = "Series1";
+            series1.YValuesPerPoint = 4;
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(1129, 168);
+            this.chart.TabIndex = 1;
+            this.chart.Text = "chart1";
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(596, 22);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(254, 28);
+            this.dateTimePicker.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1161, 598);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.accountListLabel);
+            this.Controls.Add(this.accountCntLabel);
+            this.Controls.Add(this.idLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.loginStateButton);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.axKHOpenAPI1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxKHOpenAPILib.AxKHOpenAPI axKHOpenAPI1;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button loginStateButton;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label idLabel;
+        private System.Windows.Forms.Label accountCntLabel;
+        private System.Windows.Forms.Label accountListLabel;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+    }
+}
+
---
+++ StockProject1/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace StockProject1
+{
+    public partial class Form1 : Form
+    {
+        List<ItemInfo> itemInfoList;
+        List<ItemPriceEntity> itemPriceEntityList;
+
+        Series candleSeries;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            loginButton.Click += ButtonClicked;
+            loginStateButton.Click += ButtonClicked;
+            searchButton.Click += ButtonClicked;
+
+            axKHOpenAPI1.OnEventConnect += OnEventConnect;
+            axKHOpenAPI1.OnReceiveTrData += OnReceiveTrData;
+
+            candleSeries = chart.Series["Series1"];
+            candleSeries["PriceUpColor"] = "Red";
+            candleSeries["PriceDownColor"] = "Blue";
+        }
+
+        void requestUserInfo()
+        {
+            string name = axKHOpenAPI1.GetLoginInfo("USER_NAME");
+            nameLabel.Text = name;
+
+            string id = axKHOpenAPI1.GetLoginInfo("USER_ID");
+            idLabel.Text = id;
+
+            string accountCnt = axKHOpenAPI1.GetLoginInfo("ACCOUNT_CNT");
+            accountCntLabel.Text = accountCnt;
+
+            string accountList = axKHOpenAPI1.GetLoginInfo("ACCLIST");
+            accountListLabel.Text = accountList;
+
+        }
+
+        void requestItemCodeList()
+        {
+            itemInfoList = new List<ItemInfo>();
+
+            string codeList = axKHOpenAPI1.GetCodeListByMarket("0");
+            string[] codeArray = codeList.Split(';');
+
+            for (int i = 0; i < codeArray.Length; i++)
+            {
+                string itemName = axKHOpenAPI1.GetMasterCodeName(codeArray[i]);
+                ItemInfo item = new ItemInfo(codeArray[i], itemName);
+                itemInfoList.Add(item);
+                
+            }
+        }
+
+
+        void OnReceiveTrData(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEvent e)
+        {
+            if (e.sRQName == "일별주가")
+            {
+                int count = axKHOpenAPI1.GetRepeatCnt(e.sTrCode, e.sRQName);
+
+                candleSeries.Points.Clear(); // 다른 종목 검색시 캔들이 누적되는 것을 방지하기 위해
+
+                // Y축 값 스케일링을 위해서 저가, 고가 값을 저장
+                int minValue = int.MaxValue;
+                int maxValue = int.MinValue;
+
+                for (int i = 0; i < count; i++)
+                {
+                    string 날짜 = axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "날짜").Trim();
+                    int 시가 = int.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "시가"));
+                    int 고가 = int.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "고가"));
+                    int 저가 = int.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "저가"));
+                    int 종가 = int.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "종가"));
+                    int 전일비 = int.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "전일비"));
+                    double 등락률 = double.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "등락률"));
+                    long 거래량 = long.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "거래량"));
+                    long 금액 = long.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "금액(백만)"));
+                    double 신용비 = double.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "신용비"));
+                    long 개인 = long.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "개인").Replace("--","-"));
+                    long 기관 = long.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "기관").Replace("--", "-"));
+                    long 외인수량 = long.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "외인수량").Replace("--", "-"));
+                    long 외국계 = long.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "외국계").Replace("--", "-"));
+                    long 프로그램 = long.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "프로그램").Replace("--", "-"));
+                    double 외인비 = double.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "외인비").Replace("--", "-"));
+
+                    if (시가 < 0) 시가 = -시가;
+                    if (고가 < 0) 고가 = -고가;
+                    if (저가 < 0) 저가 = -저가;
+                    if (종가 < 0) 종가 = -종가;
+
+                    //Console.WriteLine("날짜: " + 날짜 + "  종가: " + 종가);
+                    itemPriceEntityList.Add(new ItemPriceEntity(날짜, 시가, 고가, 저가, 종가, 전일비, 등락률, 거래량, 금액, 신용비, 개인, 기관, 외인수량, 외국계, 프로그램, 외인비));
+
+                    if (minValue > 저가)
+                        minValue = 저가;
+                    if (maxValue < 고가)
+                        maxValue = 고가;
+
+                    candleSeries.Points.AddXY(날짜, 고가);
+                    candleSeries.Points[i].YValues[1] = 저가;
+                    candleSeries.Points[i].YValues[2] = 시가;
+                    candleSeries.Points[i].YValues[3] = 종가;
+
+                    candleSeries.Points[i].ToolTip = "일자: " + 날짜 + "\n"
+                        + "시가: " + String.Format("{0:#,###}", 시가) + "\n"
+                        + "종가: " + String.Format("{0:#,###}", 종가) + "\n"
+                        + "고가: " + String.Format("{0:#,###}", 고가) + "\n"
+                        + "저가: " + String.Format("{0:#,###}", 저가) + "\n"
+                        + "거래량: " + String.Format("{0:#,###}", 거래량);
+
+                }
+
+                dataGridView.DataSource = itemPriceEntityList;
+
+                // 차트 스케일링
+                chart.ChartAreas["ChartArea1"].AxisY.Maximum = maxValue;
+                chart.ChartAreas["ChartArea1"].AxisY.Minimum = minValue;
+            }
+        }
+
+        void OnEventConnect(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnEventConnectEvent e)
+        {
+            if(e.nErrCode == 0)
+            {
+                //MessageBox.Show("정상적으로 로그인 되었습니다");
+                requestUserInfo();
+                requestItemCodeList();
+
+                // 검색 소스에 종목명 넣기
+                AutoCompleteStringCollection source = new AutoCompleteStringCollection();
+                for (int i = 0; i < itemInfoList.Count; i++)
+                {
+                    source.Add(itemInfoList[i].itemName);
+                }
+                searchTextBox.AutoCompleteCustomSource = source;
+
+            }
+            else if (e.nErrCode == 100)
+            {
+                MessageBox.Show("사용자 정보교환 실패");
+            }
+            else if (e.nErrCode == 101)
+            {
+                MessageBox.Show("서버접속 실패");
+            }
+            else if (e.nErrCode == 102)
+            {
+                MessageBox.Show("버전처리 실패");
+            }
+            else
+            {
+                MessageBox.Show("알 수 없는 오류 발생");
+            }
+        }
+
+
+        private void ButtonClicked(object sender, EventArgs e)
+        {
+            if (sender.Equals(loginButton))
+            {
+                axKHOpenAPI1.CommConnect();
+            }
+            else if (sender.Equals(loginStateButton))
+            {
+                int res = axKHOpenAPI1.GetConnectState();
+                if (res == 0)
+                {
+                    MessageBox.Show("로그인 되지 않았습니다.");
+                }
+                else if (res == 1)
+                {
+                    MessageBox.Show("로그인 되었습니다.");
+                }
+            }
+            else if (sender.Equals(searchButton))
+            {
+                string itemName = searchTextBox.Text;
+                DateTime dt = dateTimePicker.Value;
+                String date = dt.ToString("yyyyMMdd");
+
+                for (int i = 0; i < itemInfoList.Count; i++)
+                {
+                    if (itemName == itemInfoList[i].itemName)
+                    {
+                        itemPriceEntityList = new List<ItemPriceEntity>();
+
+                        //MessageBox.Show(itemInfoList[i].itemCode);
+                        axKHOpenAPI1.SetInputValue("종목코드", itemInfoList[i].itemCode);
+                        axKHOpenAPI1.SetInputValue("조회일자", date);
+                        axKHOpenAPI1.SetInputValue("표시구분", "1");
+
+                        axKHOpenAPI1.CommRqData("일별주가", "opt10086", 0, "5001");
+
+                        break;
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    class ItemInfo
+    {
+        public string itemCode;
+        public string itemName;
+
+        public ItemInfo(string itemCode, string itemName)
+        {
+            this.itemCode = itemCode;
+            this.itemName = itemName;
+        }
+    }
+
+    class ItemPriceEntity
+    {
+        public string 날짜 { get; set; }
+        public int 시가 { get; set; }
+        public int 고가 { get; set; }
+        public int 저가 { get; set; }
+        public int 종가 { get; set; }
+        public int 전일비 { get; set; }
+        public double 등락률 { get; set; }
+        public long 거래량 { get; set; }
+        public long 금액 { get; set; }
+        public double 신용비 { get; set; }
+        public long 개인 { get; set; }
+        public long 기관 { get; set; }
+        public long 외인수량 { get; set; }
+        public long 외국계 { get; set; }
+        public long 프로그램 { get; set; }
+        public double 외인비 { get; set; }
+
+        public ItemPriceEntity() { }
+
+        public ItemPriceEntity(string 날짜, int 시가, int 고가, int 저가, int 종가, int 전일비, double 등락률, long 거래량, long 금액, double 신용비, long 개인, long 기관, long 외인수량, long 외국계, long 프로그램, double 외인비)
+        {
+            this.날짜 = 날짜;
+            this.시가 = 시가;
+            this.고가 = 고가;
+            this.저가 = 저가;
+            this.종가 = 종가;
+            this.전일비 = 전일비;
+            this.등락률 = 등락률;
+            this.거래량 = 거래량;
+            this.신용비 = 신용비;
+            this.개인 = 개인;
+            this.기관 = 기관;
+            this.외인수량 = 외인수량;
+            this.외국계 = 외국계;
+            this.프로그램 = 프로그램;
+            this.외인수량 = 외인수량;
+            this.외인비 = 외인비;
+        }
+    }
+}
